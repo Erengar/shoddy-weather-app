13b95dbf488a26bb5018489a3510476c74c49914
Adam Kubik <kubik.adam445@gmail.com>
2023-11-04 14:33:41 +0100

    finalized setBackground logic

--- src/components/helperFunctions/setBackground.js
+++ src/components/helperFunctions/setBackground.js
@@ -1,12 +1,16 @@
 function setBackground(time, iconCode) {
     document.body.removeAttribute('class');
     if (time > '18:00:00' | time < '06:00:00') {
-        if (iconCode=== '01n') {
+        if (iconCode === '01n' | iconCode === '02n') {
             document.body.className = "night";
         } else if (iconCode=== '09n' | iconCode=== '10n' | iconCode=== '11n') {
             document.body.className = "night-rain";
         } else if (iconCode=== '03n' | iconCode=== '04n') {
             document.body.className = "night-clouds";
+        } else if (iconCode === '13n') {
+            document.body.className = "night-snow";
+        } else if (iconCode=== '50n') {
+            document.body.className = "night-mist";
         }
     } else if (iconCode=== '09d' | iconCode=== '10d' | iconCode=== '11d') {
         document.body.className = "rain";
@@ -14,6 +18,8 @@ function setBackground(time, iconCode) {
         document.body.className = "snow";
     } else if (iconCode=== '50d') {
         document.body.className = "mist";
+    } else if (iconCode=== '03d' | iconCode=== '04d') {
+        document.body.className = "clouds";
     }
 }
 
