a3a506428fcb78575b00097da7c3592daab52448
Adam Kubik <kubik.adam445@gmail.com>
2023-11-07 20:22:17 +0100

    removed redundant if form WeatherWindows

--- src/components/WeatherWindows.jsx
+++ src/components/WeatherWindows.jsx
@@ -6,41 +6,40 @@ import setBackground from './helperFunctions/setBackground';
 
 function WeatherWindows(props) {
 
-    if(JSON.stringify(props.weather) !== '{}'){
-        const time = moment().utc().add(props.weather.timezone, 's').format('HH:mm:ss');
+    const time = moment().utc().add(props.weather.timezone, 's').format('HH:mm:ss');
 
-        const iconCode = checkWeather(props.weather, time);
+    const iconCode = checkWeather(props.weather, time);
 
-        setBackground(time, iconCode);
+    setBackground(time, iconCode);
 
-        fetch(`https://openweathermap.org/img/wn/${iconCode}.png`)
-        .then(response => response.blob())
-        .then(images => {
-            const img = document.createElement('img');
-            img.src = URL.createObjectURL(images);
-            const iconElement = document.querySelector('i');
-            if (iconElement.firstChild) {
-                iconElement.replaceChild(img, iconElement.firstChild);
-            } else {
-                iconElement.appendChild(img);
-            }
-        })
-        return (
-            <>
-                <section className='weather-window weather-animation'>
-                    {time}
-                    <i>{}</i>
-                    <h1 className ='desc'>{capitalizeFirstLetter(props.weather.weather[0].description)}</h1>
-                    <h2 className ='temperature'>{props.weather.main.temp} °C</h2>
-                    <p><strong>Pressure:</strong> {props.weather.main.pressure} hPa</p>
-                    <p><strong>Humidity:</strong> {props.weather.main.humidity} %</p>
-                    <p><strong>Wind-speed:</strong> {props.weather.wind.speed} meter/sec</p>
-                </section>
-                <h1 className="nextTitle">Next 24 Hours</h1>
-                <MultipleDaysWeather multipleDaysWeather={props.multipleDaysWeather} weather={props.weather} />
-            </>
-        )
-    }
+    fetch(`https://openweathermap.org/img/wn/${iconCode}.png`)
+    .then(response => response.blob())
+    .then(images => {
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(images);
+        const iconElement = document.querySelector('i');
+        if (iconElement.firstChild) {
+            iconElement.replaceChild(img, iconElement.firstChild);
+        } else {
+            iconElement.appendChild(img);
+        }
+    })
+    return (
+        <>
+            <section className='weather-window weather-animation'>
+                {time}
+                <i>{}</i>
+                <h1 className ='desc'>{capitalizeFirstLetter(props.weather.weather[0].description)}</h1>
+                <h2 className ='temperature'>{props.weather.main.temp} °C</h2>
+                <p><strong>Pressure:</strong> {props.weather.main.pressure} hPa</p>
+                <p><strong>Humidity:</strong> {props.weather.main.humidity} %</p>
+                <p><strong>Wind-speed:</strong> {props.weather.wind.speed} meter/sec</p>
+            </section>
+            <h1 className="nextTitle">Next 24 Hours</h1>
+            <MultipleDaysWeather multipleDaysWeather={props.multipleDaysWeather} weather={props.weather} />
+        </>
+    )
+    
 }
 
 export default WeatherWindows;
